7ec15c5738f5585e152833d5dd6bf16b3a813a21
GOPI CHANDRA SHARMA <63851794+gopi261190@users.noreply.github.com>
2021-09-25 13:59:07 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".icon").click(function(){
+      $(".left").animate({
+        width: "toggle"
+      });
+    });
+  });
+
+
+$(document).ready(function(){
+    $(".close").click(function(){
+      $(".left").animate({
+        width: "toggle"
+      });
+    });
+  });
